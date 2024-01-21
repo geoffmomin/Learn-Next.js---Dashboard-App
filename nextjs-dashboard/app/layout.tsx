9a56afb0632fe2ac89c1616684ea49c3f826b45d
Geoffrey Momin <geoffmomin@gmail.com>
2024-01-21 19:30:11 +0000

    feat: Add metadata to layout component and format layout component imports

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,7 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
-
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://learn-next-js-dashboard-59miwtl4t-geoffrey-momins-projects.vercel.app'),
+};
 
 export default function RootLayout({
   children,
