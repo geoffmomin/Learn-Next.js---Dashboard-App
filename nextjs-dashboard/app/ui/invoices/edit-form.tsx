8c1c9549cfc7486e9615b72b04ecea8fa0b56951
Geoffrey Momin <geoffmomin@gmail.com>
2024-01-21 07:53:30 +0000

    Added server-side error validation and messages

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -11,6 +11,7 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { UpdateInvoice } from './buttons';
 import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,10 +20,14 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-
+  const InitialState = {
+    message: null,
+    errors: {},
+  };
+  const [state, dispatch] = useFormState(updateInvoice.bind(null, invoice.id), InitialState);
+  
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
